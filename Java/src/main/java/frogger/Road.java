c132e703801b0e45b7d88ae9d4664a61ad942e49
LaxmiNarayanaMurthyVemuri <murthyvemuri@msitprogram.net>
2024-02-06 14:57:48 +0530

    updated Road.java

--- Java/src/main/java/frogger/Road.java
+++ Java/src/main/java/frogger/Road.java
@@ -11,8 +11,12 @@ public class Road {
     public Road(boolean[] occupied) {
         this.occupied = occupied;
     }
-
-    public boolean[] getOccupied() {
-        return this.occupied;
+    
+    public boolean isOccupied(int position) {
+        return occupied[position];
+    }
+    
+    public boolean isValidPosition(int position) {
+        return position >= 0 && position < occupied.length;
     }
 }
